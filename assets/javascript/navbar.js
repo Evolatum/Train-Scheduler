3aab42d1ffd59e8745c012a2767fc20d816aeeb1
Santiago MVO <santiago.mvao@gmail.com>
2019-10-22 17:30:49 -0500

    updated portfolio link

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -1,11 +1,11 @@
 $("body").prepend(`
 <nav class="navbar navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="https://evolatum.github.io/Portfolio/portfolio.html">
+    <a class="navbar-brand" href="https://evolatum.github.io/Portfolio/index.html#linkPortfolio">
         <i class="fad fa-chevron-left d-inline-block align-middle" id="backButton" alt="Go back"></i>
         Back to portfolio
     </a>
     <span class="navbar-text" id="nameLong">
-        Santiago Martinez Vara Ortiz - xx/xx/19
+        Santiago Martinez Vara Ortiz - 18/10/19
     </span>
     <span class="navbar-text" id="nameShort">
         SMVO - 2019
